1da9a85b1315c982361e22433fcca8342e049049
Amira Mostafa <amira.mostafa@bevy.com>
2021-11-15 12:10:14 +0200

    fix: add style to errorScreen

--- src/components/ErrorScreen/index.tsx
+++ src/components/ErrorScreen/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {ErrorResponse} from '../../models';
 
 const ErrorScreen = ({errorMessage}: {errorMessage: ErrorResponse}) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>{errorMessage.message}</Text>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 export default ErrorScreen;
